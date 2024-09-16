438c149207ad13a4ab513e6ba823247bbae7d0a5
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-16 23:58:57 +0530

    Footer Section Added ➕

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import './Footer.css'
+
+function Footer(){
+    return(
+        <>
+        <div className='footer'>
+            {/* <hr /> */}
+            <div id="footer">
+                <p align='center'>
+                Designed and Developed by Abhinav Kumar
+                <br/>
+                Powered By Unsplash API
+                </p> 
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Footer
