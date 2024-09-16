f801703967917eabe7315a9a90cccd9905573ef4
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-09-16 23:58:40 +0530

    Component Added ➕

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import Navbar from './Navbar'
+import Searchbar from './Searchbar'
+import Footer from './Footer'
+import fetchRandomImages from './API_Integration'
+import Imagebox from './Imagebox'
+import Image from './Image'
+
+function App() {
+    const [value, setValue] = useState('');
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        if (value) {
+            fetchRandomImages(value, 6).then(fetchedImages => {
+                setImages(fetchedImages);
+            }).catch(error => {
+                console.error('Error fetching random images:', error);
+            });
+        }
+    }, [value]);
+
+    return (
+        <>
+            <Navbar/>
+            <Searchbar search={setValue} />
+            <Imagebox>
+                {images.map((image, index) => (
+                    <Image 
+                        key={index}
+                        url={image.url}
+                        description={image.description}
+                        downloadUrl={image.downloadUrl}
+                    />
+                ))}
+            </Imagebox>
+            <Footer/>
+        </>
+    )
+}
+
+export default App
